36beb6673c481099e3561f53be3caf348b861905
luciancioroga <lucian.cioroga@gmail.com>
2018-01-10 00:10:09 +0200

    Sesiunea 2

---
+++ sesiunea-2/src/main/java/gallery/decode/com/gallery/PreviewActivity.java
@@ -0,0 +1,43 @@
+package gallery.decode.com.gallery;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by lucian.cioroga on 1/9/2018.
+ */
+
+public class PreviewActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button mBack0, mBack1, mBack2;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preview);
+        setTitle("Preview");
+
+        mBack0 = findViewById(R.id.back_0);
+        mBack1 = findViewById(R.id.back_1);
+        mBack2 = findViewById(R.id.back_2);
+        mBack0.setOnClickListener(this);
+        mBack1.setOnClickListener(this);
+        mBack2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.back_0) {
+            setResult(0);
+            finish();
+        } else if (v.getId() == R.id.back_1) {
+            setResult(1);
+            finish();
+        } else if (v.getId() == R.id.back_2) {
+            setResult(2);
+            finish();
+        }
+    }
+}
