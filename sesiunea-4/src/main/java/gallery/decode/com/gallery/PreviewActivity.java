347248a5a49fe7db94c7fd7b9888b48fd698322a
luciancioroga <lucian.cioroga@gmail.com>
2018-03-07 16:41:29 +0200

    Sesiunea 4 - extra code

---
+++ sesiunea-4/src/main/java/gallery/decode/com/gallery/PreviewActivity.java
@@ -0,0 +1,29 @@
+package gallery.decode.com.gallery;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by lucian.cioroga on 1/9/2018.
+ */
+
+public class PreviewActivity extends AppCompatActivity {
+    private View mPreview;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preview);
+        setTitle("Preview");
+
+        mPreview = findViewById(R.id.preview);
+        int color = getIntent().getIntExtra("color", 0);
+
+        Log.d("Color", "" + color);
+        mPreview.setBackgroundColor(color);
+    }
+}
